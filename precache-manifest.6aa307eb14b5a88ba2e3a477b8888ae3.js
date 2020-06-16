6d8512fda4b102aed2831a5532369a1e8a689038
spunkiee <65862890+spunkiee@users.noreply.github.com>
2020-06-16 17:03:09 +0530

    Updates

---
+++ precache-manifest.6aa307eb14b5a88ba2e3a477b8888ae3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131ea2ebfdcf971f58344d4570fd2e66",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "b5e8093fac809e8ec2b7",
+    "url": "/react-calculator/static/css/main.22a6e7a3.chunk.css"
+  },
+  {
+    "revision": "e0b2661185035eea491d",
+    "url": "/react-calculator/static/js/2.efac12cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.efac12cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e8093fac809e8ec2b7",
+    "url": "/react-calculator/static/js/main.49929587.chunk.js"
+  },
+  {
+    "revision": "f7185cf5358719f4ecc8",
+    "url": "/react-calculator/static/js/runtime-main.db5894ed.js"
+  }
+]);
